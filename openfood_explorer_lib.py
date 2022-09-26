d47ba13c5011822b1173bb7a88304c1cb3da44a5
ekolesmana <eko@itsavirus.com>
2022-09-27 07:28:44 +0800

    updated hardcoded raddress to use right variable

--- openfood_explorer_lib.py
+++ openfood_explorer_lib.py
@@ -18,8 +18,7 @@ def explorer_get_utxos(querywallet):
     # INSIGHT_API_KOMODO_ADDRESS_UTXO = "insight-api-komodo/addrs/{querywallet}/utxo"
     INSIGHT_API_KOMODO_ADDRESS_UTXO = "insight-api-komodo/addrs/" + querywallet + "/utxo"
     try:
-        #res = requests.get(EXPLORER_URL + INSIGHT_API_KOMODO_ADDRESS_UTXO)
-        res = requests.get(EXPLORER_URL + 'insight-api-komodo/addrs/RH5dNSsN3k4wfHZ2zbNBqtAQ9hJyVJWy4r/utxo')
+        res = requests.get(EXPLORER_URL + INSIGHT_API_KOMODO_ADDRESS_UTXO)
 
         print("end explorer_get_utxos")
     except Exception as e:
