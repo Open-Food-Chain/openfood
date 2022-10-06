d7fc7fab5fffbca43baed5ec43cdd8c5e7d9da4b
ekolesmana <eko@itsavirus.com>
2022-10-06 13:51:55 +0800

    add function getbalance komodo node and unittest

---
+++ tests/test_komodo_node.py
@@ -0,0 +1,8 @@
+import pytest
+import requests
+
+from openfood_lib_dev.openfood_komodo_node import getbalance
+
+def test_get_balance():
+    response = getbalance()
+    #assert isinstance(float(response), float)
