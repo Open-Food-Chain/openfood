400a639d3f431b4b079ba9d2649d8988d21f524e
ekolesmana <eko@itsavirus.com>
2022-11-04 08:02:26 +0800

    add log level

--- openfood_log.py
+++ openfood_log.py
@@ -1,13 +1,24 @@
 import os
 import sentry_sdk
+from sentry_sdk import set_level
 
 SENTRY_SDK_DSN = str(os.environ['SENTRY_SDK_DSN'])
+ENVIRONMENT = str(os.environ['ENVIRONMENT'])
+LOGGER_LEVEL = str(os.environ['LOGGER_LEVEL'])
 
-sentry_sdk.init(
-    dsn=SENTRY_SDK_DSN,
+#SENTRY_SDK_DSN = "https://198cbc30e54d4fdf85eb5e94fe670450@o4503918670249984.ingest.sentry.io/4503918673985537"
+#SENTRY_SDK_DSN = "https://f275ccd8f45f42c9af7337c626a6d495@o237067.ingest.sentry.io/4503917202046976"
+#SENTRY_SDK_DSN = ""
+#ENVIRONMENT="production"
+#LOGGER_LEVEL="info"
 
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0
-)
+if SENTRY_SDK_DSN:
+    sentry_sdk.init(
+        dsn=SENTRY_SDK_DSN,
+        traces_sample_rate=1.0,
+        environment=str(os.environ['ENVIRONMENT'])
+    )
+
+    logger_level = LOGGER_LEVEL if LOGGER_LEVEL else "debug"
+    
+    set_level(logger_level)
