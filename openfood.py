f482a6862d8040b9affd0ca23d3e55d223220a3c
mylo <imylomylo@gmail.com>
2023-05-19 21:48:00 +0700

    JC-2566 fix cast

--- openfood.py
+++ openfood.py
@@ -1492,7 +1492,7 @@ def push_industry_foundation(name, raddress, pubkey):
 def industry_oracle_baton_update(foundation_id, baton):
     oracle = get_jcapi_foundation_oracle(foundation_id)
     oracle['baton'] = baton
-    oracle_baton_update_url = URL_openfood_API_FOUNDATION_ORACLE + oracle['id'] + "/"
+    oracle_baton_update_url = URL_openfood_API_FOUNDATION_ORACLE + str(oracle['id']) + "/"
     oracle_update_response = putWrapper(oracle_baton_update_url, oracle)
     return oracle_update_response
 
